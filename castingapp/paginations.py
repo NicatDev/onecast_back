f5116b7e3da4025ec212a5dd17f23da882b81625
unknown <nicat254memmedov@gmail.com>
2023-05-17 18:10:04 +0400

    totalpage

--- castingapp/paginations.py
+++ castingapp/paginations.py
@@ -9,6 +9,7 @@ class CustomPagination(PageNumberPagination):
     def get_paginated_response(self, data):
         current_page = int(self.request.GET.get('page', 1))
         return Response({
+            'total_pages': self.page.paginator.num_pages,
             'count': self.page.paginator.count,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
