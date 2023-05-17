a663c6b46cbe76d08032e04d1900066c44d83c95
unknown <nicat254memmedov@gmail.com>
2023-05-17 18:14:52 +0400

    agefilter

--- castingapp/filters.py
+++ castingapp/filters.py
@@ -1,13 +1,22 @@
 import django_filters
 from account.models import Profile
 
+class CustomNumericRangeFilter(django_filters.Filter):
+    def filter(self, qs, value):
+        if value:
+            value_parts = value.split(',')
+            if len(value_parts) == 2:
+                min_value, max_value = value_parts
+                return super().filter(qs, django_filters.fields.Lookup((min_value, max_value), 'range'))
+        return super().filter(qs, value)
+    
 class ProductFilter(django_filters.FilterSet):
     # category__id = django_filters.CharFilter(lookup_expr='iexact')
     # category__name = django_filters.CharFilter(lookup_expr='icontains')
     # name = django_filters.CharFilter(lookup_expr='icontains')
     # description = django_filters.CharFilter(lookup_expr='icontains')
     gender = django_filters.CharFilter(lookup_expr='icontains')
-    age = django_filters.NumericRangeFilter()         
+    age = CustomNumericRangeFilter()         
     height = django_filters.NumericRangeFilter()                                                             
     class Meta:
         model = Profile
