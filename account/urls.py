e5630c0630dd9e15ec2764133367e883badaddba
unknown <nicat254memmedov@gmail.com>
2023-05-20 17:41:32 +0400

    login url

--- account/urls.py
+++ account/urls.py
@@ -8,8 +8,8 @@ from rest_framework_simplejwt import views as jwt_views
 app_name = "accounts-api"
 
 urlpatterns = [
-    path("login/?type=talent/", TalentLoginView.as_view(), name="login"),
-    path("login/?type=company/", CompanyLoginView.as_view(), name="login"),
+    path("login/talent/", TalentLoginView.as_view(), name="login"),
+    path("login/company/", CompanyLoginView.as_view(), name="login"),
     path('api/token/',jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/',jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('talentRegister/',RegistrationView.as_view(), name='tlntrg'),
