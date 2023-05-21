5e50ccab3de4c90e7284ae0a48bc254f5566a835
unknown <nicat254memmedov@gmail.com>
2023-05-21 15:46:28 +0400

    123aa

--- account/urls.py
+++ account/urls.py
@@ -2,29 +2,36 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from account.views import CheckUsername,CompanyListView,TalentFilterPage,TalentAllFilterPage,TalentActorFilterPage,TalentChildFilterPage,TalentModelFilterPage,TalentPageView,CompanyLoginView,TalentLoginView,RegistrationView,CompanyRegisterView,HomePagePopularView,HomePageTalentsView,TalentSingleView
+from account.views import TalentSettingEditView,CheckUsername,CompanyListView,TalentFilterPage,TalentAllFilterPage,TalentActorFilterPage,TalentChildFilterPage,TalentModelFilterPage,TalentPageView,CompanyLoginView,TalentLoginView,RegistrationView,CompanyRegisterView,HomePagePopularView,HomePageTalentsView,TalentSingleView
 
 from rest_framework_simplejwt import views as jwt_views
 app_name = "accounts-api"
 
 urlpatterns = [
+    #auth
     path("login/talent/", TalentLoginView.as_view(), name="login"),
     path("login/company/", CompanyLoginView.as_view(), name="login"),
     path('api/token/',jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/',jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('talentRegister/',RegistrationView.as_view(), name='tlntrg'),
     path('companyRegister/',CompanyRegisterView.as_view(), name='cmpnyrg'),
+    path('CheckUsername/',CheckUsername.as_view(), name='CheckUsername'),
+    #homepage
     path('HomePagePopularView/',HomePagePopularView.as_view(), name='HomePagePopularView'),
     path('HomePageTalentsView/',HomePageTalentsView.as_view(), name='HomePageTalentsView'),
+    #singlepage
     path('TalentSingleView/<int:id>',TalentSingleView.as_view(), name='TalentSingleView'),
     path('TalentPageView/',TalentPageView.as_view(), name='TalentPageView'),
+    #talentcompanypage
     path('TalentModelFilterPage/',TalentModelFilterPage.as_view(), name='TalentModelFilterPage'),
     path('TalentActorFilterPage/',TalentActorFilterPage.as_view(), name='TalentActorFilterPage'),
     path('TalentChildFilterPage/',TalentChildFilterPage.as_view(), name='TalentChildFilterPage'),
     path('TalentAllFilterPage/',TalentAllFilterPage.as_view(), name='TalentAllFilterPage'),
     path('TalentFilterPage/',TalentFilterPage.as_view(), name='TalentFilterPage'),
     path('CompanyListView/',CompanyListView.as_view(), name='CompanyListView'),
-    path('CheckUsername/',CheckUsername.as_view(), name='CheckUsername'),  
+      
+    path('TalentSettingEditView/',TalentSettingEditView.as_view(), name='TalentSettingEditView'),  
+    
 ] 
 
 
